38f7bf7f3fc6f3288e5dfa02fc04785a1a62315b
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-01-09 20:47:09 +0530

    Map through State Array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,69 @@
 import './App.css';
 import Employee from './components/Employee';
 import {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 
 function App() {
+  const [employees,setEmployees] = useState(
+    [
+      {
+        "name":"Tirthraj",
+        "age":"19",
+        "dob":"7 June 2004",
+        "role":"Developer",
+        "image":"https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        "name":"Julie",
+        "age":"24",
+        "dob":"21 January 1999",
+        "role":"HR",
+        "image":"https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        "name":"Sofia",
+        "age":"25",
+        "dob":"20 June 2004",
+        "role":"Developer",
+        "image":"https://images.unsplash.com/photo-1601412436009-d964bd02edbc?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        "name":"Caleb",
+        "age":"15",
+        "dob":"2 December 2007",
+        "role":"Intern",
+        "image":"https://images.unsplash.com/photo-1544348817-5f2cf14b88c8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        "name":"Mary",
+        "age":"25",
+        "dob":"21 May 1997",
+        "role":"Developer",
+        "image":"https://images.unsplash.com/photo-1549351512-c5e12b11e283?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        "name":"Rocky",
+        "age":"22",
+        "dob":"1 Feb 2000",
+        "role":"Developer",
+        "image":"https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+    ]
+  )
   
-  const [role,setRole] = useState("dev");
-
-
   return (
     <div className="App">
     
       <>
-        <input type="text" onChange={(e) => {
-          setRole(e.target.value);
-        }} placeholder="Enter Role"></input>
-        <Employee name="Tirthraj" role={role}/>
+        <div className="flex flex-wrap flex-row justify-center gap-5 pt-5 pb-5">
+          {employees.map((employee)=>{
+            return(
+              <Employee key={uuidv4()} name={employee.name} age={employee.age} dob={employee.dob} role={employee.role} image={employee.image}/>
+            )
+          })}
+        </div>    
+
       </>
     
     </div>
--- my-app/src/components/Employee.js
+++ my-app/src/components/Employee.js
@@ -2,8 +2,17 @@ function Employee(props){
 
     return (
         <>
-            <h1>Welcome {props.name?props.name:"Employee"}</h1>
-            <p>Role: {props.role?props.role:"No Role"}</p>
+            <div className="flex flex-nowrap flex-row rounded-xl drop-shadow border-4 gap-3 p-5 pb-0">
+                <img className="w-[150px] h-[150px] aspect-square rounded-full border-1 border-black" src={props.image?props.image:"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
+                <div className="text-center font-sans p-5">
+                    <h1 className="font-bold text-center underline underline-offset-2 text-xl mb-5">{props.name?props.name:"Employee"}</h1>
+                    <p className="mt-3 mb-5"><span className="inline-block mr-5">Age: {props.age?props.age:"NA"}</span><span className="inline-block ml-5">DOB: {props.dob?props.dob:"NA"}</span></p>
+                    <p className="mb-5">Role: {props.role?props.role:"NA"}</p>
+                    <button className="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update</button>
+                </div>
+            </div>
+
+
         </>
     )
 }
